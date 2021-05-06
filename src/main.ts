c97c8de3dc49872c0487f60c03a079ca2ed38d0e
Glenou <glen@yeswedev.fr>
2021-05-06 13:40:17 +0200

    Add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { store, key } from './store';
+import './styles/vendor.css';
 
 createApp(App).use(store, key).use(router).mount('#app');
